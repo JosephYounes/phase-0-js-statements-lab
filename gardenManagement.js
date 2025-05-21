040effb1bd20088b9d28acb42a49f42c00d83834
JosephYounes <joseph.younes52@hotmail.com>
2025-05-21 13:14:49 -0400

    This is phase-0-lab

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,20 @@
-const temperature = 90;
+let temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+if (temperature > 80) {
+  console.log("Watering on");
+} else {
+  console.log("Watering off");
+}
+let newTimeOfDay = "evening";
+if (newTimeOfDay === "evening" || newTimeOfDay === "night") {
+  console.log("Lights on");
+} else {
+  console.log("Lights off");
+}
+let soilMoistureLevel = 30;
+while (soilMoistureLevel < 40) {
+  console.log(soilMoisture);
+  soilMoisture += 5;
+}
+console.log(soilMoisture);
